d1af0f3c1588e0e288e38b309f85261d9e2c644d
DenSpar <vorobev.denis@list.ru>
2020-07-06 18:46:12 +0300

    разделил старый модуль поиска на слушатель ввода и модуль поиска

--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
 import './styles.css';
-import listRepModule from '@js/listRepLoader';
-import makeRepsListModule from '@js/makeRepsList';
 import searchModule from '@js/searchInput';
-import makePaginatorModule from '@js/paginatorLoader';
+import inputListenerModule from '@js/inputListener';
 
-searchModule();
+inputListenerModule();
 
 if (localStorage.getItem('searchName')) {
     searchInput.value = localStorage.getItem('searchName');
-    let requestUrl = 'https://api.github.com/search/repositories?q=' + localStorage.getItem('searchName') + '&sort=stars&page=' + localStorage.getItem('page') + '&per_page=2'
-//заменить количество репов на 10 ^
-    makeRepsListModule(requestUrl, 2000)
-    .then(dataList => {
-        listRepModule(dataList.listItems);
-        console.log(dataList.totalCount);
-        makePaginatorModule(dataList.totalCount);
-    }).catch(err => console.log(err));
+    searchModule(localStorage.getItem('searchName'), localStorage.getItem('page'));
 } else {
-    let mostPopRepsURL = 'https://api.github.com/search/repositories?q=stars%3A%3E0&sort=stars&order=desc&page=1&per_page=1';
-//заменить количество репов на 10 ^
-    makeRepsListModule(mostPopRepsURL, 2000)
-    .then(dataList => {
-        listRepModule(dataList.listItems);
-    }).catch(err => console.log(err));
+    searchModule('stars%3A%3E0', localStorage.getItem('page'));
 };
--- src/js/searchInput.js
+++ src/js/searchInput.js
@@ -1,28 +1,16 @@
 import listRepModule from '@js/listRepLoader';
 import makeRepsListModule from '@js/makeRepsList';
 import makePaginatorModule from '@js/paginatorLoader';
-let searchInput = document.querySelector('#searchInput');
 
-let searchModule = function() {    
-    //создаем событие для ввода запроса
-    searchInput.addEventListener ('keydown', function (evt) {
-        if (evt.keyCode === 13) {
-            if (searchInput.value === '') {
-                searchInput.placeholder = 'репозиторий не указан :(';
-            } else {                
-                let requestURL = 'https://api.github.com/search/repositories?q=' + searchInput.value + '&sort=stars&page=1&per_page=1';
-                //заменить количество репов на 10 ^
-                localStorage.setItem('searchName', searchInput.value.toString());
-                localStorage.setItem('page', '1');
-                makeRepsListModule(requestURL, 2000)
-                .then(dataList => {
-                    listRepModule(dataList.listItems);
-                    console.log(dataList.totalCount);
-                    makePaginatorModule(dataList.totalCount);
-                }).catch(err => console.log(err));
-            };
-        };
-    });
+let searchModule = function(searchName, searchPage) { 
+    let requestURL = 'https://api.github.com/search/repositories?q=' + searchName + '&sort=stars&order=desc&page=' + searchPage + '&per_page=2';   
+    //заменить количество репов на 10 ^
+    makeRepsListModule(requestURL, 2000)
+    .then(dataList => {
+        listRepModule(dataList.listItems);
+        console.log(dataList.totalCount);
+        makePaginatorModule(dataList.totalCount);
+    }).catch(err => console.log(err));
 };
 
 export default searchModule;
